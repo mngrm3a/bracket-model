ae8cde70163b241b9a967e845985b6bba4b5481d
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-05-01 23:36:11 +0200

    Add HoleProfile dataclass

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import functools
 import os
 import sys
@@ -5,10 +6,19 @@ from build123d import *
 from ocp_vscode import *
 
 
+@dataclass
+class HoleSection:
+    radius: float
+    depth: float
+
+
+HoleProfile = list[HoleSection]
+
+
 class Socket(BasePartObject):
     def __init__(
         self,
-        hole_profile: list[tuple[float, float]],
+        hole_profile: HoleProfile,
         wall_thickness: float,
         edge_chamfer: float = 0,
         rotation: RotationLike = (0, 0, 0),
@@ -16,7 +26,9 @@ class Socket(BasePartObject):
         mode: Mode = Mode.ADD,
     ):
         max_inner_radius, self.socket_depth = functools.reduce(
-            lambda a, x: (max(a[0], x[0]), a[1] + x[1]), hole_profile, (0, 0)
+            lambda a, x: (max(a[0], x.radius), a[1] + x.depth),
+            hole_profile,
+            (0, 0),
         )
         self.socket_size = 2 * (max_inner_radius + wall_thickness)
 
@@ -29,8 +41,8 @@ class Socket(BasePartObject):
 
             for s in hole_profile:
                 with BuildSketch((part_b.faces() | Plane.XZ > Axis.Y)[-2:][0]):
-                    Circle(s[0])
-                extrude(amount=-s[1], mode=Mode.SUBTRACT)
+                    Circle(s.radius)
+                extrude(amount=-s.depth, mode=Mode.SUBTRACT)
 
         part_b.part.label = "Socket"
         super().__init__(part_b.part, rotation, align, mode)
@@ -39,7 +51,7 @@ class Socket(BasePartObject):
 class RazorBracket(BasePartObject):
     def __init__(
         self,
-        hole_profile: list[tuple[float, float]],
+        hole_profile: HoleProfile,
         wall_thickness: float,
         slot_size: float,
         slot_offset: float = 0,
@@ -63,11 +75,11 @@ class RazorBracket(BasePartObject):
             extrude(until=Until.LAST, both=True, mode=Mode.SUBTRACT)
 
             if edge_chamfer > 0:
-                x_edges = (
-                    part_b.edges().filter_by(Axis.X).group_by(Axis.Z, reverse=True)
-                )
-                chamfer((x_edges[0] > Axis.Y)[1:3], edge_chamfer / 2)
-                chamfer((x_edges[1] > Axis.Y)[0:2], edge_chamfer / 2)
+                # x_edges = (
+                #     part_b.edges().filter_by(Axis.X).group_by(Axis.Z, reverse=True)
+                # )
+                # chamfer((x_edges[0] > Axis.Y)[1:3], edge_chamfer / 2)
+                # chamfer((x_edges[1] > Axis.Y)[0:2], edge_chamfer / 2)
 
                 xz_faces = part_b.faces() | Plane.XZ > Axis.Y
                 chamfer(xz_faces[0].edges(), edge_chamfer)
@@ -79,10 +91,10 @@ class RazorBracket(BasePartObject):
 
 
 SOCKET_HOLE_SECTIONS = [
-    (7, 4),  # nut top
-    (4.5, 3),  # nut bottom
-    (2, 2),  # screw / thread
-    (8.5, 1.5),  # cup
+    HoleSection(7, 4),  # nut top
+    HoleSection(4.5, 3),  # nut bottom
+    HoleSection(2, 2),  # screw / thread
+    HoleSection(8.5, 1.5),  # cup
 ]
 
 result = RazorBracket(
