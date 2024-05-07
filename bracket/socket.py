deeb7fc832bf0b1b410904bc3ac3c9777868794e
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-05-07 21:42:45 +0200

    Import build123d qualified in socket.py

--- bracket/socket.py
+++ bracket/socket.py
@@ -1,7 +1,7 @@
 import collections.abc
 import functools
 from dataclasses import dataclass
-from build123d import *
+import build123d as bd
 
 
 @dataclass
@@ -28,35 +28,36 @@ class HoleProfile(collections.abc.Iterable[HoleSection]):
         )
 
 
-class Socket(BasePartObject):
+class Socket(bd.BasePartObject):
     def __init__(
         self,
         hole_profile: HoleProfile,
         wall_thickness: float,
         sides_chamfer: float = 0,
-        rotation: RotationLike = (0, 0, 0),
-        align: Align | tuple[Align, Align, Align] = Align.CENTER,
-        mode: Mode = Mode.ADD,
+        rotation: bd.RotationLike = (0, 0, 0),
+        align: bd.Align | tuple[bd.Align, bd.Align, bd.Align] = bd.Align.CENTER,
+        mode: bd.Mode = bd.Mode.ADD,
     ):
+        self.hole_profile = hole_profile
         max_inner_radius, self.socket_depth = hole_profile.outer_profile()
         self.socket_size = 2 * (max_inner_radius + wall_thickness)
 
-        with BuildPart() as part_b:
-            with BuildSketch(Plane.XZ):
-                r = Rectangle(self.socket_size, self.socket_size)
+        with bd.BuildPart() as part_b:
+            with bd.BuildSketch(bd.Plane.XZ):
+                r = bd.Rectangle(self.socket_size, self.socket_size)
                 if sides_chamfer:
-                    chamfer(r.vertices(), sides_chamfer)
-            extrude(amount=self.socket_depth)
+                    bd.chamfer(r.vertices(), sides_chamfer)
+            bd.extrude(amount=self.socket_depth)
 
             for s in hole_profile:
-                with BuildSketch((part_b.faces() | Plane.XZ > Axis.Y)[-2:][0]):
-                    Circle(s.radius)
-                extrude(amount=-s.depth, mode=Mode.SUBTRACT)
+                with bd.BuildSketch((part_b.faces() | bd.Plane.XZ > bd.Axis.Y)[-2:][0]):
+                    bd.Circle(s.radius)
+                bd.extrude(amount=-s.depth, mode=bd.Mode.SUBTRACT)
 
-        RigidJoint(
+        bd.RigidJoint(
             "socket_joint",
             part_b.part,
-            Plane((part_b.faces() < Axis.Y).last).location,
+            bd.Plane((part_b.faces() < bd.Axis.Y).last).location,
         )
         part_b.part.label = "Socket"
         super().__init__(part_b.part, rotation, align, mode)
